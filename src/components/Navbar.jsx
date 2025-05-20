87a09327a54facf4b1c210c1b71e219746ed76b5
Tushar Pardhe <tusharpardhe2018@gmail.com>
2025-05-20 22:46:29 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+
+const navItems = [
+    {
+        name: "Home", href: "#hero"
+    },
+    {
+        name: "About", href: "#about"
+    },
+    {
+        name: "Skills", href: "#skills"
+    },
+    {
+        name: "Projects", href: "#project"
+    },
+    {
+        name: "Contact", href: "#contact"
+    },
+];
+
+export const Navbar = () => {
+    return <div></div>
+};
